b90b3951a30c63d1aa323914b8af79e13502c6f2
Alon Ronder <alonr110@gmail.com>
2024-04-14 08:34:59 +0300

    Rename test file

--- src/test/java/SpreadSheetServiceTest.java
+++ src/test/java/SpreadSheetServiceTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SpreadSheetServiceTestUpdate {
+public class SpreadSheetServiceTest {
     private SpreadsheetService service;
     @BeforeEach
     void setUp() {
